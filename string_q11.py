59a9efb0f31964f1bcc90cbd1240449e6ce48402
Shikhar28-web <vermashikhar380@gmail.com>
2024-11-27 21:21:36 +0530

    Delete string_q11.py

--- string_q11.py
+++
@@ -1,9 +0,0 @@
-# Write a function to check if one string is a rotation of another string.
-
-def is_rotation(s1, s2):
-    if len(s1) != len(s2):
-        return False
-    return s2 in (s1 + s1)
-
-
-print(is_rotation("waterbottle", "erbottlewat")) 
