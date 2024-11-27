da90f6a2f54042ad56ba32eafb8de41980e2cabe
Shikhar28-web <vermashikhar380@gmail.com>
2024-11-27 21:24:36 +0530

    Delete string_q09.py

--- string_q09.py
+++
@@ -1,7 +0,0 @@
-# Write a function to generate all substrings of a string.
-
-def all_substrings(s):
-    return [s[i:j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)]
-
-
-print(all_substrings("abc")) 
